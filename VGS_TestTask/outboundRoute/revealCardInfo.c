904e516a2b77c377b6bcc36c294cc476a19f4295
charleycc168 <75466246+charleycc168@users.noreply.github.com>
2020-12-08 01:49:19 -1000

    Update revealCardInfo.c

--- VGS_TestTask/outboundRoute/revealCardInfo.c
+++ VGS_TestTask/outboundRoute/revealCardInfo.c
@@ -9,7 +9,7 @@ printf("Content-type:text/html\r\n\r\n");
 printf("<html> <head></head><body>");
 printf("<h5>Reveal your redacted card info</h5>");
 
-printf("<form action=\"http://192.168.200.107/cgi-bin/reveal\" method=\"post\">");
+printf("<form action=\"<directory>/cgi-bin/reveal\" method=\"post\">"); //need to update this directory to your environment before compile
 
 printf("<input type=text name=cardHolder placeholder=\"Redacted card holder name \">");
 printf("<br>");
