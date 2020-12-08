6b4b88f3f26c1eca758bf7d3c9bbbe79dfcd5f0a
charleycc168 <75466246+charleycc168@users.noreply.github.com>
2020-12-08 01:56:36 -1000

    Update requestCardInfo.c

--- VGS_TestTask/inboundRoute/requestCardInfo.c
+++ VGS_TestTask/inboundRoute/requestCardInfo.c
@@ -8,7 +8,7 @@ printf("Content-type:text/html\r\n\r\n");
 printf("<html> <head></head><body>");
  printf("<h5>Redact your credit card info</h5>");
 
-printf("<form action=\"http://192.168.200.107/cgi-bin/redact\" method=\"post\">");
+printf("<form action=\"http://<directory>/cgi-bin/redact\" method=\"post\">"); // need to update this <directory> to your environment before compile
 printf("<input type=text name=cardHolder placeholder=\"Card holder name \">");
 printf("<br>");
 printf("<input type=text name=cardNumber placeholder=\"16 digits credit card number \">");
